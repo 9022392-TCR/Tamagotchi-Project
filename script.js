d2f513607c0b2c235e4f9bd6e9290199a613592c
9022392 <9022392@student.zadkine.nl>
2026-01-19 10:23:33 +0100

    began work emotions
    -added text to display emotions
    -makes game stop when one of the stats reaches 0

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+let game = true;
+
 let hunger = 100;
 let sleep = 100;
 let fun = 100;
+
+const thoughts = [
+    "I'm happy",
+    "I'm hungry",
+    "I'm bored",
+    "I'm sleepy",
+    "I'm sad"
+];
 
 document.getElementById("music").loop = true;
 document.getElementById("music").play();
@@ -12,14 +22,11 @@ function statUpdate(){
 }
 statUpdate();
 
-if (hunger === 70){
-    document.getElementById('chat').innerHTML = "feed me";
-}
-
-
 
 function feed(){
+    if (game == true){
     hunger += 20;
+    }
     if (hunger > 100){
         hunger = 100;
     }
@@ -27,7 +34,9 @@ function feed(){
 }
 
 function nap(){
+    if (game == true){
     sleep += 20;
+    }
     if (sleep > 100){
         sleep = 100
     }
@@ -35,21 +44,62 @@ function nap(){
 }
 
 function play(){
-    fun += 20;
+    if (game == true){
+        fun += 20;
+    }
     if (fun > 100){
         fun = 100;
     }
     statUpdate()
 }
 
-document.getElementById("chat").innerHTML = "hello world";
-
 setInterval(time, 3000);
 function time(){
-    hunger -= 1;
-    sleep -= 1;
-    fun -= 1;
-    age += 1;
+    if (fun == 0){
+        game = false;
+    }if (hunger == 0){
+        game == false;
+    }if (sleep == 0){
+        game == false;
+    }
 
-    statUpdate()
+    if (game === true){
+        hunger -= 1;
+        sleep -= 1;
+        fun -= 1;
+
+        statUpdate()
+    }
 }
+
+const c = document.getElementById("chat");
+const e = document.getElementById("entity");
+let emote = 0;
+
+setInterval(exp, 100);
+function exp(){
+    if (hunger <= 70){
+        if (hunger <= 30){
+            c.textContent = thoughts[4];
+        }else{
+        c.textContent = thoughts[1];
+        
+        }
+    }else if(fun <= 70){
+        if (fun <= 30){
+            c.textContent = thoughts[4]
+        }else{
+        c.textContent = thoughts[2];
+        }
+    }else if (sleep <= 70){
+        if (sleep <= 30){
+            c.textContent = thoughts[4]
+        }else{
+        c.textContent = thoughts[3];
+        }
+    }else{
+        c.textContent = thoughts[0];
+        c.src = 'sprites/tamagotchi.png';
+    }
+}
+
